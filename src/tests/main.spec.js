514f68dd094402cf6550144e28144e643d30b75d
Nikita Bondarev <nikita.bondarev1807@gmail.com>
2022-10-06 02:24:40 +0300

    Fix bugs

--- src/tests/main.spec.js
+++ src/tests/main.spec.js
@@ -6,17 +6,27 @@ import chai from 'chai';
 
 describe('showMessage()', () => {
   it('should work alert with correct text', () => {
-    const text = 'Hello!';
+    const originAlert = alert;
+
+    window.alert = (text) => {
+      window.alert.wasCalled = true;
+      window.alert.argument = text;
+    }
+
+    showMessage('Hello');
     
-    expect(showMessage(text)).to.equal(showMessage(text));
-  });
-});
+    expect(alert.wasCalled).to.be.true;
+    expect(alert.argument).to.equal('Hello');
+
+    window.alert = originAlert;
+  })
+})
 
 describe('getDay()', () => {
   it('should return day matching by array index', () => {
     expect(getDay()).to.equal(days[new Date().getDay()]);
-  });
-});
+  })
+})
 
 describe('getAdultUsers()', () => {
   it('should return array of objects with age greater than 18', () => {
@@ -30,8 +40,19 @@ describe('getAdultUsers()', () => {
   })
 })
 
+const length = users.length;
+const middleUser = Math.round(length / 2);
+
 describe('getRandomUsers()', () => {
-  it('should return a random array with users', () => {
-    expect(getRandomUsers(users).length).not.to.equal(users.length)
+  it('should return false if the argument is not transferred', () => {
+    expect(getRandomUsers()).to.be.false;
+  })
+
+  it('should return the array of its length to divide by 2', () => {
+    expect(JSON.stringify(getRandomUsers(users))).to.equal(JSON.stringify(users.slice(0, middleUser)));
+  })
+
+  it('Should return the array from the middle to the end', () => {
+    expect(JSON.stringify(getRandomUsers(users))).to.equal(JSON.stringify(users.slice(middleUser, length)));
   })
 })
